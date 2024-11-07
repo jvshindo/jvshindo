0c6a958991d30eac59035df896c28839fbcf5efe
jvshindo <jvshindo@gmail.com>
2024-11-07 09:48:03 -0300

    Add files via upload

--- src/database.h
+++ src/database.h
@@ -7,3 +7,12 @@ public:
     static Database& getInstance();
     bool saveItemEnchantment(int itemId, Enchantment* enchantment); // Função para salvar encantamento
 };
+
+// Definition for DBResult
+class DBResult {
+public:
+    // Add necessary methods and properties here
+};
+
+// Definition for DBResult_ptr
+using DBResult_ptr = std::shared_ptr<DBResult>;
